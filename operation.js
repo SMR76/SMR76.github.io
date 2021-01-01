2947fa25f2f82400940db26f21befec7adabd486
SMR <seyyedmortezarazavi76@gmail.com>
2021-01-01 23:51:35 +0330

    added separate style sheet and js file.

---
+++ operation.js
@@ -0,0 +1,24 @@
+
+function initializeSkills() {    
+    var langList = [
+        {lang:'C++',master: 11},
+        {lang:'C++',master: 11},
+        {lang:'C++',master: 11}
+    ];
+
+
+
+
+    var skills = $("skills")                
+var skillformat = 
+'<div class="row align-top">'
++'<div class="col-2 h-100 align-text-top">' + + '</div>'
++'<div class="col-sm-10 col-lg-5 pt-2"><div class="progress">'                
++'<div class="progress-bar" role="progressbar" style="width: ' + + '%" aria-valuenow="'+ +'" aria-valuemin="0" aria-valuemax="100"></div>
++'</div></div></div>'
+    
+}
+
+$(document).ready(() => {
+    initializeSkills();
+});
